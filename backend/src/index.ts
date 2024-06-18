dfa3287c230d3f22008055ee7bc2de3fdeca866b
Illamapalooza <juliusbaliling25@gmail.com>
2024-06-18 20:30:50 +0800

    Added CRUD operations for notes and users

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -8,7 +8,7 @@ import { sql } from 'drizzle-orm';
 
 import { aiNoteRoute } from './app/aiNote/aiNote.controller';
 // import db from './database';
-import { notes } from './schema';
+import { notes, users } from './schema';
 import { Pool } from 'pg';
 import { drizzle } from 'drizzle-orm/node-postgres';
 
@@ -51,35 +51,127 @@ const startServer = async () => {
  // APIS for the database
 
  app.use('/api/get-users', async (req, res) => {
-  // Todo get users
+  try {
+   const allUsers = await db.select().from(users);
+   res.json(allUsers);
+
+   console.log(allUsers);
+  } catch (error) {
+   console.error('Error getting users', error);
+   res.status(500).json({ error: 'An error occurred' });
+  }
  });
 
  app.use('/api/insert-users', async (req, res) => {
   // Todo insert users
- });
+  try {
+   const { name, email, password } = req.body;
 
+   console.log(name, email, password);
+
+   const insertUser = await db
+    .insert(users)
+    .values([{ name, email, password }]);
+   res.json(insertUser);
+  } catch (error) {
+   console.log('Error inserting user', error);
+  }
+ });
  app.use('/api/get-user/:id', async (req, res) => {
   // Todo get user by id
+  try {
+   const { id } = req.params;
+   const user = await db
+    .select()
+    .from(users)
+    .where(sql`${users.id} = ${id}`);
+   res.json(user);
+  } catch (error) {
+   console.log('Error getting user by id', error);
+  }
  });
 
  app.use('/api/update-user/:id', async (req, res) => {
-  // Todo update user
+  try {
+   const { id } = req.params;
+
+   const { name, email, password } = req.body;
+   const user = await db
+    .update(users)
+    .set({ name, email, password })
+    .where(sql`${users.id} = ${id}`);
+   res.json(user);
+  } catch (error) {
+   console.log('Error updating user', error);
+  }
  });
 
  app.use('/api/get-notes', async (req, res) => {
+  try {
+   const allNotes = await db.select().from(notes);
+   res.json(allNotes);
+
+   console.log(allNotes);
+  } catch (error) {
+   console.error('Error updating user', error);
+   res.status(500).json({ error: 'An error occurred' });
+  }
   // Todo get notes
  });
 
  app.use('/api/insert-notes', async (req, res) => {
+  try {
+   const { topic, image_url, content } = req.body;
+
+   console.log(topic, image_url, content);
+
+   const insertNote = await db
+    .insert(notes)
+    .values([{ topic, image_url, content }]);
+   res.json(insertNote);
+
+   console.error(insertNote);
+  } catch (error) {
+   console.log('Error inserting note', error);
+  }
   // Todo insert notes
  });
 
  app.use('/api/update-note/:id', async (req, res) => {
-  // Todo update notes
+  try {
+   const { id } = req.params;
+   const { topic, image_url, content } = req.body;
+   const updateNote = await db
+    .update(notes)
+    .set({ topic, image_url, content })
+    .where(sql`${notes.id} = ${id}`);
+   res.json(updateNote);
+  } catch (error) {
+   console.log('Error updating note', error);
+  }
  });
 
  app.use('/api/delete-note/:id', async (req, res) => {
   // Todo delete notes
+  try {
+   const { id } = req.params;
+   const deleteNote = await db.delete(notes).where(sql`${notes.id} = ${id}`);
+
+   res.json(deleteNote);
+  } catch (error) {
+   console.log('Error deleting note', error);
+  }
+ });
+
+ app.use('/api/delete-user/:id', async (req, res) => {
+  // Todo delete user
+  try {
+   const { id } = req.params;
+   const deleteUsers = await db.delete(users).where(sql`${users.id} = ${id}`);
+   res.json(deleteUsers);
+  } catch (error) {
+   console.log('Error deleting user', error);
+  }
  });
 
  app.listen(port, host, () => {
